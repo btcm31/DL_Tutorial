613d008fe2ac136eb808608ff817ffe7f46010c3
btcm31 <congminhabcd@gmail.com>
2021-07-16 22:51:14 +0700

    l1

--- L1/l1.py
+++ L1/l1.py
@@ -8,38 +8,40 @@ Created on Mon Feb 18 22:06:34 2019
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#numOfPoint = 30
-#noise = np.random.normal(0,1,numOfPoint).reshape(-1,1)
-#x = np.linspace(30, 100, numOfPoint).reshape(-1,1)
-#N = x.shape[0]
-#y = 15*x + 8 + 20*noise
-#plt.scatter(x, y)
 
 data = pd.read_csv('data_linear.csv').values
 N = data.shape[0]
-x = data[:, 0].reshape(-1, 1)
-y = data[:, 1].reshape(-1, 1)
-plt.scatter(x, y)
-plt.xlabel('mét vuông')
-plt.ylabel('giá')
+X = data[:,0].reshape(-1,1)
+y = data[:,1].reshape(-1,1)
+""" plt.plot(X,y)
+plt.xlabel('Dien tich')
+plt.ylabel('Gia')
+plt.show() """
 
-x = np.hstack((np.ones((N, 1)), x))
+X = np.hstack((np.ones((N,1)),X))
 
-w = np.array([0.,1.]).reshape(-1,1)
+numOfIter = 100
+cost = np.zeros((3,1))
+lr_lst = [0.00001]
+y_pred = np.zeros((3,N))
+w = ""
+for idx,lr in enumerate(lr_lst):
+    w = np.array([0.,1.]).reshape(-1,1)
+    for i in range(1,numOfIter):
+        r = np.dot(X,w) - y #np.dot  => dot((M*N),(N*R)) = array(M*R)\
+        w[0] = w[0] - lr * np.sum(r)
+        w[1] = w[1] - lr * np.sum(np.multiply(r,X[:,1].reshape(-1,1)))
+    a = np.dot(X,w)-y
+    cost[idx] += 1/2 * np.sum(a*a)# * operation carries out element-wise multiplication on array elements.
+    y_pred[idx] = np.dot(X,w).reshape(1,-1)
 
-numOfIteration = 100
-cost = np.zeros((numOfIteration,1))
-learning_rate = 0.000001
-for i in range(1, numOfIteration):
-    r = np.dot(x, w) - y
-    cost[i] = 0.5*np.sum(r*r)
-    w[0] -= learning_rate*np.sum(r)
-    # correct the shape dimension
-    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1].reshape(-1,1)))
-    print(cost[i])
-predict = np.dot(x, w)
-plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+plt.plot(y_pred[0],'--')
+plt.plot(y,'+')
+plt.legend()
 plt.show()
+print(cost)
+print(w)
+
 
 x1 = 50
 y1 = w[0] + w[1] * 50
